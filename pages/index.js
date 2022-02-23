198bf8d33fcbb7fdf4213e08af67791f8991ff49
LArroyoS <50588683+LArroyoS@users.noreply.github.com>
2022-02-23 17:23:02 -0600

    cambio 4

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          cambio 3
+          cambio 4
           <code className={styles.code}>pages/index.js</code>
         </p>
 
