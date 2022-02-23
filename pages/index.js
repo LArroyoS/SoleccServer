b096ae9b880eeb5879149933e7947c88240e445e
LArroyoS <50588683+LArroyoS@users.noreply.github.com>
2022-02-23 17:17:53 -0600

    otro cambio

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          va de nuevo
+          otro cambio
           <code className={styles.code}>pages/index.js</code>
         </p>
 
