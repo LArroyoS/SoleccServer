cc95e50b8e64553de9c5f0c307ac36c626bb32a9
LArroyoS <50588683+LArroyoS@users.noreply.github.com>
2022-02-23 17:13:48 -0600

    netlify 2

--- pages/index.js
+++ pages/index.js
@@ -17,8 +17,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Editar
-          Get started by editing{' '}
+          va de nuevo
           <code className={styles.code}>pages/index.js</code>
         </p>
 
