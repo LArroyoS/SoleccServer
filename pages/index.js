81a63904cad57c511ab512186143a0d9591b52fc
LArroyoS <50588683+LArroyoS@users.noreply.github.com>
2022-02-24 10:25:39 -0600

    prueba formulario 1

--- pages/index.js
+++ pages/index.js
@@ -22,9 +22,9 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
+          <a href="./api/formulario.js" className={styles.card}>
             <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <p>Prueba Formulario.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
