d58dbbb1d7b7c4f855c1bdb3e5814e1e30ea58ef
LArroyoS <50588683+LArroyoS@users.noreply.github.com>
2022-02-24 10:39:18 -0600

    formulario 1.2

--- pages/api/formulario.js
+++ pages/api/formulario.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../../styles/Home.module.css'
 
-export default function Formulario() {
+export default function Formulario(req, res) {
   return (
     <div className={styles.container}>
       <Head>
