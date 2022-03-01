3bc41fbd203c57f14353e94c64b357d59b62b27c
LArroyoS <50588683+LArroyoS@users.noreply.github.com>
2022-03-01 12:33:45 -0800

    pruebaPeticionFormulario1.3

--- pages/index.js
+++ pages/index.js
@@ -12,13 +12,15 @@ export default function Home() {
     <div>
       <h1>Formulario</h1>
       <label>Tipo de peticion</label>
+      <br></br>
       <select name="peticion" onChange={cambio}>
         <option value="">--Seleccione un tipo formulario--</option>
         <option value="GET">GET</option>
-        <option value="POST">GET</option>
-        <option value="PUT">GET</option>
-        <option value="DELETE">GET</option>
+        <option value="POST">POST</option>
+        <option value="PUT">PUT</option>
+        <option value="DELETE">DELETE</option>
       </select>
+      <br></br>
       <button onClick={click}>enviar</button>
     </div>
   )
