edb236957ba2a7a0dbb9c2ee1cf1d6dfc3f9bb75
LArroyoS <50588683+LArroyoS@users.noreply.github.com>
2022-03-01 15:07:40 -0800

    pruebaPeticionFormulario2.0.3

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 
 export default function Home() {
-  const [peticion, setPeticion] = useState("");
-  const [respuesta, setRespuesta] = useState({});
+  const [peticion, setPeticion] = useState("DEFAULT");
+  const [respuesta, setRespuesta] = useState({ peticion: "default" });
   const cambio = ({ target }) => {
     setPeticion(target.value);
   }
@@ -20,7 +20,7 @@ export default function Home() {
       <label>Tipo de peticion</label>
       <br></br>
       <select name="peticion" onChange={cambio}>
-        <option value="">--Seleccione un tipo formulario--</option>
+        <option value="DEFAULT">--Seleccione un tipo formulario--</option>
         <option value="GET">GET</option>
         <option value="POST">POST</option>
         <option value="PUT">PUT</option>
@@ -28,6 +28,8 @@ export default function Home() {
       </select>
       <br></br>
       <button onClick={click}>enviar</button>
+      <br></br>
+      <h3>Peticion { peticion }</h3>
       <br></br>
       <h2>Respuesta { JSON.stringify(respuesta) }</h2>
     </div>
