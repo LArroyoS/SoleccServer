cea39a52ed319db8c38458d5fcbcda905642f218
LArroyoS <50588683+LArroyoS@users.noreply.github.com>
2022-03-03 13:32:41 -0600

    pruebaComponente1.2.1

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PruebaPeticion from "../componentes/pruebaPeticion";
-import PruebaPeticion from "../componentes/pruebaFormulario";
+import PruebaFormulario from "../componentes/pruebaFormulario";
 
 export default function Home() {
 
