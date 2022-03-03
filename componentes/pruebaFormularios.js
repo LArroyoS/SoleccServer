3bd5b842eee17bb0fabdd35c47c49da122fa5700
LArroyoS <50588683+LArroyoS@users.noreply.github.com>
2022-03-03 13:30:42 -0600

    pruebaComponentes1.2.0

---
+++ componentes/pruebaFormularios.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function PruebaFormulario() {
+
+  return (
+    <h1>Prueba Formulario</h1>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import PruebaPeticion from "../componentes/pruebaPeticion";
+import PruebaPeticion from "../componentes/pruebaFormulario";
 
 export default function Home() {
 
   return (
-    <PruebaPeticion></PruebaPeticion>
+    <div>
+      <PruebaPeticion></PruebaPeticion>
+      <PruebaFormulario></PruebaFormulario>
+    </div>
   )
 }
