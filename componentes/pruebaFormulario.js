4e04b70850524acbae249cf0c3a70f1f3fb5d69f
LArroyoS <50588683+LArroyoS@users.noreply.github.com>
2022-03-03 15:14:57 -0600

    formulario1.0.0

--- componentes/pruebaFormulario.js
+++ componentes/pruebaFormulario.js
@@ -1,10 +1,22 @@
 import React from "react";
 
 export default function PruebaFormulario() {
-
+  const cambio = () => {
+    
+  }
   return (
     <div>
       <h1>Prueba Formulario</h1>
+      <label>Tipo de peticion</label>
+      <select name="peticion" onChange={cambio}>
+        <option value="Nula">--Seleccione un tipo formulario--</option>
+        <option value="GETALL">GET</option>
+        <option value="GET">GET</option>
+        <option value="POSTALL">POST</option>
+        <option value="POST">POST</option>
+        <option value="PUT">PUT</option>
+        <option value="DELETE">DELETE</option>
+      </select>
     </div>
   )
 }
