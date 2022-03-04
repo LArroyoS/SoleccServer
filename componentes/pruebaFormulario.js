93e2ce1e48362c0401ef1a7104714295250b3d5f
LArroyoS <50588683+LArroyoS@users.noreply.github.com>
2022-03-04 12:04:42 -0600

    pruebaFormulario1.5.0

--- componentes/pruebaFormulario.js
+++ componentes/pruebaFormulario.js
@@ -7,12 +7,18 @@ export default function PruebaFormulario() {
   const cambio = () => {
     alert("cambio");
   }
+  const opciones = () => {
+    return modelo.map((value,index) => {
+      return <opcion value={value}>{value}</opcion>
+    });
+  }
   return (
     <div>
       <h1>Prueba Formulario</h1>
       <label>Tipo de peticion</label>
       <select name="peticion" onChange={cambio}>
         <option value="Nula">--Seleccione un tabla--</option>
+        { opciones() }
       </select>
       <h4>tablas: { JSON.stringify(tablas) }</h4>
       <h4>modelo: { JSON.stringify(modelo) }</h4>
