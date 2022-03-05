93c417c3d5814626e8b86cf0ad1fc989989f31e0
Leonardo <larroyos1@toluca.tecnm.mx>
2022-03-05 11:59:12 -0600

    pruebaMongo2.0.3

--- componentes/pruebaMongo.js
+++ componentes/pruebaMongo.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Link from 'next/link'
 
 export default function PruebaMongo() {
   return (
     <div>
       <h1>Mongo</h1>
+      <Link href="/api/mongodb">
+        <a>Ir a pruebaMongo</a>
+      </Link>
     </div>
   )
 }
