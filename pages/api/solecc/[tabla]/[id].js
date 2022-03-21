69dcf352d3fb9035dae82957309516eec0c313ff
LArroyoS <50588683+LArroyoS@users.noreply.github.com>
2022-03-21 13:30:19 -0600

    QuitarMiddleware

--- pages/api/solecc/[tabla]/[id].js
+++ pages/api/solecc/[tabla]/[id].js
@@ -3,24 +3,9 @@ import ObtenerModelo from "../../../../modelos/obtenerModelo";
 
 dbConexionSolecc();
 
-// Helper method to wait for a middleware to execute before continuing
-// And to throw an error when an error happens in a middleware
-function runMiddleware(req, res, fn) {
-    return new Promise((resolve, reject) => {
-      fn(req, res, (result) => {
-        if (result instanceof Error) {
-          return reject(result)
-        }
-  
-        return resolve(result)
-      })
-    })
-}
-
 //------------------------------------------------------------------------------------
 //Petición
 export default async(req, res) => {
-    await runMiddleware(req, res, cors)
 
     const { method, body, query } = req;
     const tabla = query["tabla"];
--- pages/api/solecc/[tabla]/index.js
+++ pages/api/solecc/[tabla]/index.js
@@ -3,22 +3,7 @@ import ObtenerModelo from "../../../../modelos/obtenerModelo";
 
 dbConexionSolecc();
 
-// Helper method to wait for a middleware to execute before continuing
-// And to throw an error when an error happens in a middleware
-function runMiddleware(req, res, fn) {
-    return new Promise((resolve, reject) => {
-      fn(req, res, (result) => {
-        if (result instanceof Error) {
-          return reject(result)
-        }
-  
-        return resolve(result)
-      })
-    })
-}
-
 export default async (req, res) => {
-    await runMiddleware(req, res, cors)
 
     const { method, body, query } = req;
     const tabla = query["tabla"];
