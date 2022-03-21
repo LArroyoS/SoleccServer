a2baa2218be05ca66175c2310f1965d848dbdf5c
LArroyoS <50588683+LArroyoS@users.noreply.github.com>
2022-03-21 13:25:30 -0600

    QuitarCors

--- pages/api/solecc/[tabla]/[id].js
+++ pages/api/solecc/[tabla]/[id].js
@@ -1,13 +1,7 @@
 import dbConexionSolecc from "../../../../utilidades/conexionSolecc";
 import ObtenerModelo from "../../../../modelos/obtenerModelo";
-import cors from "cors";
 
 dbConexionSolecc();
-
-// Initializing the cors middleware
-const cors = Cors({
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'HEAD'],
-})
 
 // Helper method to wait for a middleware to execute before continuing
 // And to throw an error when an error happens in a middleware
