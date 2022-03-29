5b43ab001143d7e84a87d854f545ed1f6277f105
LArroyoS <50588683+LArroyoS@users.noreply.github.com>
2022-03-29 15:05:33 -0600

    funcionaPruebaPDF?1.3

--- pages/api/pruebaPDF/index.js
+++ pages/api/pruebaPDF/index.js
@@ -6,7 +6,6 @@ const createPDF = () => {
     doc.text("miPDF", 10, 10);
     const out = doc.output();
 
-    return "PDF";
     const base64PDF = 'data:application/pdf;base64,'+btoa(out);
     return base64PDF;
 }
