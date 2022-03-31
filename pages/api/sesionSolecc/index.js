392b1b21035f7238bdf8ead68c626785f171e667
LArroyoS <50588683+LArroyoS@users.noreply.github.com>
2022-03-31 14:03:55 -0600

    Sesion1.0

---
+++ pages/api/sesionSolecc/index.js
@@ -0,0 +1,27 @@
+import dbConexionSolecc from "../../../../utilidades/conexionSolecc";
+import ObtenerModelo from "../../../../modelos/obtenerModelo";
+
+dbConexionSolecc();
+
+export default async (req, res) => {
+
+    const { method, body } = req;
+    const tabla = "usuarios";
+    const Obj = ObtenerModelo.Objeto(tabla);
+
+    switch (method) {
+    //------------------------------------------------------------------------------------
+    //POST/PUT
+        case "POST": case "PUT":
+            const find = await Obj.find(body)
+                .then((obj) => { res.status(200).json( { status: obj })})
+                .catch((error) => { res.status(200).json({ status: false} )});
+            break;
+    //------------------------------------------------------------------------------------
+    //Metodo desonocido
+        default:
+            res.status(400).json({ success: false });
+            break;
+    }
+
+}
