d47f0420841df221d1364a7a18245b02a32051a2
LArroyoS <50588683+LArroyoS@users.noreply.github.com>
2022-07-10 14:18:26 -0500

    sesion1.2.3

--- pages/api/sesionSolecc/index.js
+++ pages/api/sesionSolecc/index.js
@@ -8,14 +8,15 @@ export default async (req, res) => {
     const { method, body } = req;
     const Obj1 = ObtenerModelo.Objeto("usuarios");
     const Obj2 = ObtenerModelo.Objeto("tipo_usuarios");
-    let status = {};
+    let status = { status: null };
     switch (method) {
     //------------------------------------------------------------------------------------
     //POST/PUT
         case "POST": case "PUT":
-            const find = await Obj1.findOne(body)
-                .then((obj) => { res.status(200).json( { status: obj })})
-                .catch((error) => { res.status(200).json({ status: error} )});
+            await Obj1.findOne(body)
+                .then((obj) => { status = { status: obj } } )
+                .catch((error) => { status = { status: false } } );
+            res.status(200).json(status);
             break;
     //------------------------------------------------------------------------------------
     //Metodo desonocido
