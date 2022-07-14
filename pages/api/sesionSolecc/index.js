d8435e849b4a47fd050f4fff9e5da38811c0e024
LArroyoS <50588683+LArroyoS@users.noreply.github.com>
2022-07-13 19:32:50 -0500

    CORS

--- pages/api/sesionSolecc/index.js
+++ pages/api/sesionSolecc/index.js
@@ -5,6 +5,12 @@ dbConexionSolecc();
 
 export default async (req, res) => {
 
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    
     const { method, body } = req;
     const Obj1 = ObtenerModelo.Objeto("usuarios");
     const Obj2 = ObtenerModelo.Objeto("tipo_usuarios");
--- pages/api/solecc/[tabla]/index.js
+++ pages/api/solecc/[tabla]/index.js
@@ -5,6 +5,12 @@ dbConexionSolecc();
 
 export default async (req, res) => {
 
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    
     const { method, body, query } = req;
     const tabla = query["tabla"];
     const Obj = ObtenerModelo.Objeto(tabla);
